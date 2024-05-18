7d386f37d0f32112eff76de36264a1591f5198e3
FumihikoTsutsuguchi <fumisamu927@gmail.com>
2024-05-18 23:37:38 +0900

    [add]Cart addメソッド追加

--- src/resources/views/user/show.blade.php
+++ src/resources/views/user/show.blade.php
@@ -18,26 +18,23 @@
                             </h2>
                             <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{ $product->name }}</h1>
                             <p class="leading-relaxed">{{ $product->information }}</p>
-                            <div class="flex justify-around items-center mt-10">
-                                <div>
-                                    <span class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}</span><span class="text-sm">円(税込)</span>
-                                </div>
-                                <div class="flex items-center">
-                                    <div class="relative">
-                                        {{-- <select name="quantity" type="number"
-                                            class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 text-base pl-3 pr-10">
-                                            <option>SM</option>
-                                            <option>M</option>
-                                            <option>L</option>
-                                            <option>XL</option>
-                                        </select> --}}
-                                        <label for="quantity">数量</label>
-                                        <input type="number" name="quantity" id="quantity" class="rounded border appearance-none border-gray-300" value="0" min="0">
+                            <form action="{{ route('user.cart.add')}}" method="post">
+                                @csrf
+                                <input type="hidden" name="product_id" value="{{ $product->id}}">
+                                <div class="flex justify-around items-center mt-10">
+                                    <div>
+                                        <span class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}</span><span class="text-sm">円(税込)</span>
                                     </div>
-                                    <button
-                                        class="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded ml-4">カートに入れる</button>
+                                    <div class="flex items-center">
+                                        <div class="relative">
+                                            <label for="quantity">数量</label>
+                                            <input type="number" name="quantity" id="quantity" class="rounded border appearance-none border-gray-300" value="0" min="0">
+                                        </div>
+                                        <button
+                                            class="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded ml-4">カートに入れる</button>
+                                    </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
