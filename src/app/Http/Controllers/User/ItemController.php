ec2daf49c1564c20c2466180c7630e83901d9949
FumihikoTsutsuguchi <fumisamu927@gmail.com>
2024-06-07 21:56:57 +0900

    [add]検索フォーム キーワード検索追加

--- src/app/Http/Controllers/User/ItemController.php
+++ src/app/Http/Controllers/User/ItemController.php
@@ -36,6 +36,7 @@ class ItemController extends Controller
         // dd($request);
         $products = Product::availableItems()
         ->selectCategory($request->category ?? '0')
+        ->searchKeyword($request->keyword)
         ->sortOrder($request->sort)
         ->paginate($request->pagination ?? '20');
 
