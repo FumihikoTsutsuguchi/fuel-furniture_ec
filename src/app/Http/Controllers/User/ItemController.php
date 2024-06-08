90bd6551775807240158f280b6311cf88536c12b
FumihikoTsutsuguchi <fumisamu927@gmail.com>
2024-06-08 11:41:03 +0900

    [add]キューとジョブの追加

--- src/app/Http/Controllers/User/ItemController.php
+++ src/app/Http/Controllers/User/ItemController.php
@@ -11,6 +11,7 @@ use Closure;
 use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -35,8 +36,10 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
-        Mail::to('test@test.com')
-        ->send(new TestMail());
+        // Mail::to('test@test.com')
+        // ->send(new TestMail());
+
+        SendThanksMail::dispatch(); 
 
         $products = Product::availableItems()
         ->selectCategory($request->category ?? '0')
