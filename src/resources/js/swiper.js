b121f20fd200c706e42741c8a7eab755e2841bb9
FumihikoTsutsuguchi <fumisamu927@gmail.com>
2024-06-15 00:04:25 +0900

    [add]showページ Swiper導入

---
+++ src/resources/js/swiper.js
@@ -0,0 +1,26 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+
+const swiper = new Swiper(".swiper", {
+
+    // direction: 'vertical',
+    modules: [Navigation, Pagination],
+    loop: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
--- src/resources/views/user/show.blade.php
+++ src/resources/views/user/show.blade.php
@@ -11,24 +11,44 @@
                 <div class="p-6 text-gray-900">
                     <div class="md:flex md:justify-around">
                         <div class="md:w-1/2">
-                            <x-thumbnail filename="{{ $product->imageFirst->filename ?? '' }}" type="products" />
+
+                            <div class="swiper">
+                                <div class="swiper-wrapper">
+                                    @foreach ([$product->imageFirst, $product->imageSecond, $product->imageThird, $product->imageFourth] as $image)
+                                        @if ($image && $image->filename !== null)
+                                            <div class="swiper-slide">
+                                                <img src="{{ asset('https://cf.fuel-furniture.com/products/' . $image->filename) }}" alt="">
+                                            </div>
+                                        @endif
+                                    @endforeach
+                                </div>
+                                <div class="swiper-pagination"></div>
+                                <div class="swiper-button-prev"></div>
+                                <div class="swiper-button-next"></div>
+                                <div class="swiper-scrollbar"></div>
+                            </div>
+
                         </div>
                         <div class="md:w-1/2 ml-4">
                             <h2 class="text-sm title-font text-gray-500 tracking-widest">{{ $product->category->name }}
                             </h2>
                             <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{ $product->name }}</h1>
                             <p class="leading-relaxed">{{ $product->information }}</p>
-                            <form action="{{ route('user.cart.add')}}" method="post">
+                            <form action="{{ route('user.cart.add') }}" method="post">
                                 @csrf
-                                <input type="hidden" name="product_id" value="{{ $product->id}}">
+                                <input type="hidden" name="product_id" value="{{ $product->id }}">
                                 <div class="flex justify-around items-center mt-10">
                                     <div>
-                                        <span class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}</span><span class="text-sm">円(税込)</span>
+                                        <span
+                                            class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}</span><span
+                                            class="text-sm">円(税込)</span>
                                     </div>
                                     <div class="flex items-center">
                                         <div class="relative">
                                             <label for="quantity">数量</label>
-                                            <input type="number" name="quantity" id="quantity" class="rounded border appearance-none border-gray-300" value="0" min="0">
+                                            <input type="number" name="quantity" id="quantity"
+                                                class="rounded border appearance-none border-gray-300" value="0"
+                                                min="0">
                                         </div>
                                         <button
                                             class="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded ml-4">カートに入れる</button>
