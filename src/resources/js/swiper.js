1b30ca7f2394eef91d9dadeb836723bf9727ac00
FumihikoTsutsuguchi <fumisamu927@gmail.com>
2024-06-15 22:37:16 +0900

    [add]スライダー追加

--- src/resources/js/swiper.js
+++ src/resources/js/swiper.js
@@ -1,26 +1,26 @@
-import Swiper from "swiper";
-import { Navigation, Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
 
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".slider-thumbnail", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
 
-    // direction: 'vertical',
-    modules: [Navigation, Pagination],
+const swiper2 = new Swiper(".slider", {
     loop: true,
-
+    spaceBetween: 10,
     pagination: {
         el: ".swiper-pagination",
+        type: "progressbar",
     },
-
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-
-    scrollbar: {
-        el: ".swiper-scrollbar",
+    thumbs: {
+        swiper: swiper,
     },
 });
